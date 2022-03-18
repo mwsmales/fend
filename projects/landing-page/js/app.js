db11fb749af3d25da30b957089f92c5cdd67f697
Michael S <michael.smales@gmail.com>
2022-03-18 20:41:13 +0000

    updated app.js - dynamically populate navbar elements

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -39,7 +39,24 @@
 */
 
 // build the nav
+// get sections from the DOM
+var sections = document.getElementsByTagName('section');
+var navSections = []; // to store the navbar titles
+for (var i = 0; i < sections.length; i++) {
+    let SectionTitle = sections[i].dataset.nav;
+    navSections.push(SectionTitle);
+};
 
+// Add navbar sections to the HTML
+var new_ui
+navList = document.getElementById('navbar__list');
+
+for (var i = 0; i < navSections.length; i++) {
+    new_ui = document.createElement('li');
+    new_ui.innerText = navSections[i];
+    new_ui.className = 'menu__link';
+    navList.appendChild(new_ui);
+}
 
 // Add class 'active' to section when near top of viewport
 
